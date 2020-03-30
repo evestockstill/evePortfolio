c706942519fe77ba0d421edea31387e60c534920
Eve Stockstill <evestockstillpdx@gmail.com>
2020-03-30 13:57:56 -0700

    init

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faUser, faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import  Resume  from '../components/resume/Resume';
+import MainPage from '../components/mainPage/MainPage';
+
+library.add(fab, faUser, faAngleDown);
+
+export default function App() {
+  return (
+    <>
+      <Router>
+        <Route exact path='/' component={MainPage} />
+        <Route path='/resume' component={Resume} />
+       
+      </Router>
+    </>
+  );
+}
+//component={() => <Redirect to={{ pathname: '/YourRoute' }} />} />
---
+++ src/components/header/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Header.scss';
+import mp4 from '../assets/glitter.mp4';
+import Nav from '../nav/Nav';
+
+const Header = () => {
+  return (
+    <>
+      <div className='wrapper'>
+        <div className='background'>
+          <header className='header'>
+            <Nav />
+            <div className='bg-video'>
+              <video className='bg-video__content' autoPlay muted loop>
+                <source src={mp4} type='video/mp4' alt='video' />
+                your browser not supported
+              </video>
+            </div>
+            <span className='background-border'></span>
+            <div id='wrapper' className='animation'>
+              <span className='first-wrapper'>
+                <h2 className='first'>Eve</h2>
+              </span>
+              <span className='last-wrapper'>
+                <h2 className='text' data='.'>
+                  Stockstill
+                </h2>
+              </span>
+              <span className='title'>Full Stack Developer</span>
+            </div>
+          </header>
+          {/* <div>
+            <Project />
+          </div> */}
+        </div>
+      </div>
+    </>
+  );
+};
+export default Header;
+
+
