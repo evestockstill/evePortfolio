18f72e3a6e1f0696e9f56cbecb72ab8e4330c2a5
Eve Stockstill <evestockstillpdx@gmail.com>
2020-04-01 03:11:33 -0700

    changing about me

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -28,42 +28,45 @@ const AboutMe = () => {
         </div>
         <h1 className='greeting'>Hello</h1>
       </header>
-      <div className='title-text' id='about-title-text'>
-        <h1>Let's talk about me</h1>
-      </div>
-      <div className='center'>
-        <div className='about-wrapper'>
-          <section className='comp'>
-            <p className='about-text'>
-              <img src={pool} alt='photo 1' className='img1'></img> I spent 20 years being an active listener and excellent
-              advice giver all while making fabulous cocktails. I believe in
-              good customer service and the ability to collaborate and enjoy
-               coworkers. The flexibility of bartending paved the way for me to
+
+      <section className='section-aboutMe'>
+        <div className='big-picture'></div>
+        
+        <div className='about'>
+          <figure className='about__shape'>
+            <img src={pool} alt='photo 1' className='about__img'></img>
+            <figcaption className='about__caption'>Eve</figcaption>
+          </figure>
+          <div className='about__text'>
+            <h3 className='heading-tertiary u-margin-bottom-small'>
+              Lets talk about me
+            </h3>
+            <p>
+              I spent 20 years being an active listener and excellent advice
+              giver all while making fabulous cocktails. I believe in good
+              customer service and the ability to collaborate and enjoy
+              coworkers. The flexibility of bartending paved the way for me to
               pursue my passion for billiards reaching professional status. I
               dedicated years of practice to win state championships, and even
               got to travel for some professional pool tournaments. These days I
-              play in local leagues, spending with time with teammates. I
-              enjoying sharing my knowledge, good sportsmanship, and love of the &nbsp;
-              <img src={isla} alt='photo 1' className='img2'></img>
-              game through example and advice. I even started up a meetup for
-              software developers to network and play pool! Software Engineers
-              talent pool. I look forward to being able to start that back up.
-              When not working on new code I enjoy spoiling my dog Isla. She
-              holds all my attention and we like going to parks and hiking
-              trails. We went on a 6 month road trip together camping and
-              swimming along the way. I enjoy
-creative development with a drive for front end 
-              development.I like component style architect with Sass or PostCSS
-to build reusable styles throughout a project.I have projects
-using React, Redux, Node.js, mongoDB.My best language is
-JavaScript but currently branching out with some online courses.I
-enjoy reading and learning new style techniques to make clean and
-interesting designs using best practices, and user experience in
-  mind.
+              play in local leagues. I enjoying sharing my knowledge, good
+              sportsmanship, and love of the game through example and advice. I
+              even started up a meetup for software developers to network and
+              play pool! Software Engineers talent pool. I look forward to being
+              able to start that back up. When not working on new code I enjoy
+              spoiling my dog Isla. She holds all my attention and we like going
+              to parks and hiking trails, and camp trips. I enjoy creative
+              development with a drive for front end development.I like
+              component style architect with Sass or PostCSS to build reusable
+              styles throughout a project.I have projects using React, Redux,
+              Node.js, mongoDB.My best language is JavaScript but currently
+              branching out with some online courses.I enjoy reading and
+              learning new style techniques to make clean and interesting
+              designs using best practices, and user experience in mind.
             </p>
-          </section>
+          </div>
         </div>
-      </div>
+      </section>
     </>
   );
 };
